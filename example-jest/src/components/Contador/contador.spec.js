661ce6e21936eb68ba67ce4951cf85d81c505f3f
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2022-12-28 08:57:15 -0300

    test: criado testes para verificar a ação dos botões de incremenatar e decrementar

--- example-jest/src/components/Contador/contador.spec.js
+++ example-jest/src/components/Contador/contador.spec.js
@@ -12,6 +12,80 @@ describe("Testes no componente contador", () => {
 
         // Espera que o elemento esteja presente no documento
         expect(tituloDoContador).toBeInTheDocument();
-
     });
+
+    it("deve ter o botão incrementar", () => {
+        render(<Contador />);
+
+        // Recupera o botão pela regra: o botão que contém o nome incrementar
+        const botaoIncrementar = screen.getByRole("button", {
+            name: /incrementar/i,
+        });
+
+        expect(botaoIncrementar).toHaveClass("button--increment");
+    });
+
+    it("deve ter o botão decremetar", () => {
+        render(<Contador />);
+
+        // Recupera o botão pela regra: o botão que contém o nome incrementar
+        const botaoIncrementar = screen.getByRole("button", {
+            name: /decrementar/i,
+        });
+
+        expect(botaoIncrementar).toHaveClass("button--decrement");
+    });
+
+    it("deve surigir na tela o 1 quando clicar no botão incrementar", () => {
+        render(<Contador />);
+
+        // Recupera o botão pela regra: o botão que contém o nome incrementar
+        const botaoIncrementar = screen.getByRole("button", {
+            name: /incrementar/i,
+        });
+
+        // Espero que não tenha nenhum elemento com texto 1 na tela
+        expect(screen.queryByText("1")).toBeNull();
+
+        // Ação de clicar no botão incrementar
+        userEvent.click(botaoIncrementar);
+
+        // Recupera o elemento que aparece como contador na tela
+        const textoContador = screen.getByText("1");
+
+        // Espero que esse elemento esteja aparecendo
+        expect(textoContador).toBeInTheDocument();
+
+        // Espero que esse elemento tenha o texto 1
+        expect(textoContador.textContent).toEqual("1");
+
+        // Clicando novamento no botão
+        userEvent.click(botaoIncrementar);
+
+        // Espero que o texto tenha o número 2
+        expect(textoContador.textContent).toEqual("2");
+    });
+
+    it("deve surigir na tela o -1 quando clicar no botão decrementar", () => {
+        render(<Contador />);
+
+        const botaoDecrementar = screen.getByRole("button", {
+            name: /decrementar/i,
+        });
+
+        expect(screen.queryByText("-1")).toBeNull();
+
+        userEvent.click(botaoDecrementar);
+
+        const textoContador = screen.getByText("-1");
+
+        expect(textoContador).toBeInTheDocument();
+
+        expect(textoContador.textContent).toEqual("-1");
+
+        userEvent.click(botaoDecrementar);
+
+        expect(textoContador.textContent).toEqual("-2");
+    });
+
 });
