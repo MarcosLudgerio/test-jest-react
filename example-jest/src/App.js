823cd106bdb4261a7e6e2dc50d161c7c1f7142d3
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2022-12-28 08:37:59 -0300

    projeto teste iniciado

---
+++ example-jest/src/App.js
@@ -0,0 +1,9 @@
+import Contador from "./components";
+import './index.css';
+function App() {
+  return (
+    <Contador />
+  );
+}
+
+export default App;
---
+++ example-jest/src/components/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./index.css";
+
+export default function Contador() {
+
+    const [counter, setCounter] = useState(0);
+
+    function getModifier() {
+        if (counter > 0) return "counter__title--increment";
+        if (counter < 0) return "counter__title--decrement";
+        return "";
+    }
+    return (
+        <div className="counter">
+            <h1 className={`counter__title ${getModifier()}`}>{counter}</h1>
+            <section className="buttons">
+                <button
+                    onClick={() => setCounter(counter + 1)}
+                    className="button button--increment"
+                >
+                    incrementar
+                </button>
+                <button
+                    onClick={() => setCounter(counter - 1)}
+                    className="button button--decrement"
+                >
+                    decrementar
+                </button>
+            </section>
+        </div>
+    );
+}
